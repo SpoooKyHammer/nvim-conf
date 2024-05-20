ddaa5c3747350958f2531bf0ebf9478d7a9cf02f
SpoooKyHammer <spookyhammer2300@gmail.com>
2024-05-20 19:45:43 +0530

    ensure install for java, python, js

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,6 @@
 local options = {
-  ensure_installed = { "lua" },
-
+  ensure_installed = { "lua", "java", "python", "javascript" },
+  auto_install = true,
   highlight = {
     enable = true,
     use_languagetree = true,
